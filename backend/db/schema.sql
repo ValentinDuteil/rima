ba2e5e26399135ca19f0f69c03fe592d9c2b5601
ValentinDuteil <contactvalentinduteil@gmail.com>
2025-12-23 22:54:29 +0100

    ajout du drop table if exist

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS conjugations;
+DROP TABLE IF EXISTS translations;
+DROP TABLE IF EXISTS user_verbs;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS verbs;
+
 CREATE TABLE users
 (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -12,19 +18,6 @@ CREATE TABLE verbs
     "group" VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE translations
-(
-    id SERIAL PRIMARY KEY NOT NULL,
-    verb_id INT NOT NULL,
-    language VARCHAR(50) NOT NULL,
-    translation VARCHAR(50) NOT NULL,
-
-    CONSTRAINT fk_translations_verbs
-        foreign key (verb_id)
-        REFERENCES verbs(id)
-        ON DELETE CASCADE
-);
-
 CREATE TABLE conjugations
 (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -36,6 +29,19 @@ CREATE TABLE conjugations
     form VARCHAR(50),
 
     CONSTRAINT fk_conjugations_verbs
+        foreign key (verb_id)
+        REFERENCES verbs(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE translations
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    verb_id INT NOT NULL,
+    language VARCHAR(50) NOT NULL,
+    translation VARCHAR(50) NOT NULL,
+
+    CONSTRAINT fk_translations_verbs
         foreign key (verb_id)
         REFERENCES verbs(id)
         ON DELETE CASCADE
