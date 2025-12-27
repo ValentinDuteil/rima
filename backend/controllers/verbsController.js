8d97a9a88bf7f56f781c42f5a9fb9078a032e8f3
ValentinDuteil <contactvalentinduteil@gmail.com>
2025-12-27 16:19:54 +0100

    ajout des import et export dans index.js, ajout de getVerbById dans verbsController, teste dans le navigateur ok

--- backend/controllers/verbsController.js
+++ backend/controllers/verbsController.js
@@ -9,3 +9,17 @@ export async function getAllVerbs(req, res) {
         res.status(500).json({error: 'Erreur Serveur'});
     }
 }
+
+export async function getVerbById(req, res) {
+    try {
+        const id = req.params.id;
+        const verb = await pool.query(`SELECT * FROM verbs WHERE id = $1`, [id]);
+
+        if (verb.rows === undefined || verb.rows.length === 0) {
+            return res.status(404).json({ error: 'Verbe non trouvé' });
+        } return res.json(verb.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Erreur Serveur'});
+    }
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 import verbsRoutes from './verbs.js';
+import { Router } from 'express';
 
 const router = Router();
 
 router.use('/verbs', verbsRoutes);
+
+export default router;
