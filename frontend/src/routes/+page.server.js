e3312ff6abdc975c6b2ab99299a8c62b24c629c7
ValentinDuteil <contactvalentinduteil@gmail.com>
2025-12-28 18:37:00 +0100

    ajout d'un tri par ordre alphabétique en français pour afficher la liste de verbes  de la premièer page

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,16 +1,21 @@
 import { PUBLIC_API_URL } from '$env/static/public';
 
-export async function load() {
+export async function load({ url }) {
   try {
     const API_URL = PUBLIC_API_URL || 'http://localhost:3000';
-    const response = await fetch(`${API_URL}/api/verbs?sort=greek`);
+
+    const sortParam = url.searchParams.get('sort') || 'greek';
+
+    const response = await fetch(`${API_URL}/api/verbs?sort=${sortParam}`);
 
     if (!response.ok) {
       throw new Error('Erreur API');
     }
 
     const verbs = await response.json();
-    return { verbs };  // Passe les données à +page.svelte
+    return { 
+      verbs, // Passe les données à +page.svelte
+      currentSort: sortParam };  // dont le tri actuel
 
   } catch (error) {
     console.error('Erreur chargement verbes:', error);
