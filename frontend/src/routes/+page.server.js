0ca5bebc464aacc814b17cd9c810eb41937b6289
ValentinDuteil <contactvalentinduteil@gmail.com>
2025-12-28 18:12:40 +0100

    ajout de la variable d'environnement à page.server.js

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,5 +1,8 @@
+import { PUBLIC_API_URL } from '$env/static/public';
+
 export async function load() {
-  const response = await fetch('http://localhost:3000/api/verbs');
+  const API_URL = PUBLIC_API_URL || 'http://localhost:3000';
+  const response = await fetch(`${API_URL}/api/verbs?sort=greek`);
   const verbs = await response.json();
   return { verbs };  // Passe les données à +page.svelte
 }
