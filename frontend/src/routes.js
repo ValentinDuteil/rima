357d753a536dee2a6c0765bc5361ecafab7b45c5
ValentinDuteil <contactvalentinduteil@gmail.com>
2025-12-28 15:46:35 +0100

    migration vers sveltekit ok pour un meilleur routing

--- frontend/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './pages/Home.svelte';
-import VerbDetail from './pages/VerbDetail.svelte';
-
-const routes = {
-  '/': Home,
-  '/verbs/:id': VerbDetail,
-};
-
-export default routes;
