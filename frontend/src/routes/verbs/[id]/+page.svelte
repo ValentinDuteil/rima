8718a928a1550a70d3361b1386d8e6a9ae9fd63a
ValentinDuteil <contactvalentinduteil@gmail.com>
2026-01-11 00:59:43 +0100

    modification de la bdd, ajout de french conjugations comme table

--- frontend/src/routes/verbs/[id]/+page.svelte
+++ frontend/src/routes/verbs/[id]/+page.svelte
@@ -4,3 +4,24 @@ let { data } = $props();
 </script>
 
 <h1>Détails du verbe</h1>
+
+<div class="verb-header">
+  <div class="verb-greek">{data.verb.greek}</div>
+  <div class="verb-translation">{data.verb.translation}</div>
+  <div class="verb-group">Groupe : {data.verb.group}</div>
+</div>
+
+<h2>Conjugaisons</h2>
+
+{#if data.conjugations && data.conjugations.length > 0}
+  <div class="conjugations">
+    {#each data.conjugations as conj}
+      <div class="conj-item">
+        <span class="person">{conj.person}</span>
+        <span class="form">{conj.form}</span>
+      </div>
+    {/each}
+  </div>
+{:else}
+  <p>Aucune conjugaison disponible</p>
+{/if}
