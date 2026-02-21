1c2f988566274daef6a2647bfce165ed3c615168
ValentinDuteil <contactvalentinduteil@gmail.com>
2026-02-21 16:16:33 +0100

    ajout de loginSchema dans validators.js

--- backend/src/middlewares/validators.js
+++ backend/src/middlewares/validators.js
@@ -1,11 +1,18 @@
 import { z } from 'zod';
 
 const messageError = "Votre mot de passe doit contenir au moins 8 caractères, une majuscule, et un caractère spécial"
+const messageErrorEmpty = "Les champs ne peuvent pas être vides"
 
 export const signupSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   password: z.string()
     .min(8, messageError)
     .regex(/[A-Z]/, messageError)
     .regex(/[!@#$%^&*]/, messageError)
+});
+
+export const loginSchema = z.object({
+  email: z.email(),
+  password: z.string()
+    .min(1, messageErrorEmpty)
 });
